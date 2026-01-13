fafc0af8b6bff57ba01581cebd01fd0d0f8596d3
BangZaky0029 <bangzaky0029@gmail.com>
2026-01-13 13:59:12 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/generator-surat-laporan-bisnis/',
+  base: '/generator-surat/generator-surat-laporan-bisnis/',
   server: {
     port: 5176,
     open: true,
